cb189987cf5dce290a1c404a424d6c626697a18d
Logan Geoffrion <45701086+Lgeoffrion@users.noreply.github.com>
2019-03-04 21:25:24 -0600

    working code and minor styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,17 +6,22 @@ var surveyResults = [
     {
       "name": "Average Joe",
       "photo": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-dodgeball-a-true-underdog-story-vince-vaughn.jpg",
-      "scores": ["3","3","3","3","3","3","3","3","3","3"]
+      "scores": [3,3,3,3,3,3,3,3,3,3]
+      // "scores": ["3","3","3","3","3","3","3","3","3","3"]
     },
     {
       "name": "The Night King",
       "photo": "https://www.cheatsheet.com/wp-content/uploads/2017/08/Night-king-Battle-Scene-640x319.png",
-      "scores": ["1","1","1","1","1","1","1","1","1","1"]
+      "scores": [1,1,1,1,1,1,1,1,1,1]
+
+      // "scores": ["1","1","1","1","1","1","1","1","1","1"]
     },
     {
       "name": "Superman",
       "photo": "https://static.comicvine.com/uploads/scale_medium/13/132327/6507037-28872490_1638064799604695_1250122498385004714_n.jpg",
-      "scores": ["5","5","5","5","5","5","5","5","5","5"]
+      "scores": [5,5,5,5,5,5,5,5,5,5]
+
+      // "scores": ["5","5","5","5","5","5","5","5","5","5"]
     }
   ]
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@
 //Pull data from friends.js
 // ===============================================================================
 
-var surveyResults = require('../data/friends.js');
+var surveyResults = require('../data/friends');
 
 // ===============================================================================
 // ROUTING
@@ -16,41 +16,21 @@ app.get("/api/friends", function(req, res) {
 });
 
 
-// ************************************
-// Push To Friend Array
-// ************************************
-
-  app.post("api/friends", function(req, res) {
-    //grabs the new friend's scores to compare with friends in surveyResults array
-    var newFriendScores = req.body.scores;
-    var scoresArray = [];
-    var friendCount = 0;
-    var bestMatch = 0;
-
-    //runs through all current friends in list
-    for(var i=0; i<surveyResults.length; i++){
-      var scoresDiff = 0;
-      //run through scores to compare friends
-      for(var j=0; j<newFriendScores.length; j++){
-        scoresDiff += (Math.abs(parseInt(surveyResults[i].scores[j]) - parseInt(newFriendScores[j])));
-      }
-
-      //push results into scoresArray
-      scoresArray.push(scoresDiff);
-    }
-
-    //after all friends are compared, find best match
-    for(var i=0; i<scoresArray.length; i++){
-      if(scoresArray[i] <= scoresArray[bestMatch]){
-        bestMatch = i;
-      }
-    }
-
-    //return bestMatch data
-    var bff = surveyResults[bestMatch];
-    res.json(bff);
-
-    //pushes new submission into the friendsList array
-    surveyResults.push(req.body);
-  });
+app.post("/api/friends", function(req, res) {
+  var newFriendScore = req.body.scores;
+  var scoredArr = [];
+  var total = 0;
+    for (var i = 0; i < surveyResults.length; i++){
+     var FriendArr = surveyResults[i].scores;
+     for (var x = 0; x < newFriendScore.length; x++) {
+       total += (Math.abs(parseInt(FriendArr[x]) - parseInt(newFriendScore[x])));
+     }
+    scoredArr.push(total);
+    total = 0;
+  }
+  var bestMatchScore = Math.min.apply(null, scoredArr);
+  var key = scoredArr.indexOf(bestMatchScore);
+  surveyResults.push(req.body);
+  res.json(surveyResults[key]);
+});
 };
